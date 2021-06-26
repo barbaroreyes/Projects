bbc8dad7dbc620922f4b413e4ab5e1f9c70cfe9d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-26 08:39:28 -0500

    added-new state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react'
+import Amplify,{API} from 'aws-amplify'
+import awsconfi from './aws-exports'
+import {listProjects} from './graphql/queries'
 import './App.css';
+Amplify.configure(awsconfi)
 
 function App() {
+  const [project,setProject] = useState([])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>Header</>
+      <input type='text' placeholder='url'/>
     </div>
   );
 }
